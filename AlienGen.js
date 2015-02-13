fe263aaf953c6ba4bf7a7d3fabed18c5ce348834
edvella <git@edvella.com>
2015-02-13 19:09:05 +0100

    moved script to its own file

---
+++ AlienGen.js
@@ -0,0 +1,33 @@
+var c = document.getElementById("alienCanvas");
+var ctx = c.getContext("2d");
+
+var generator = {
+    numPixels: 10,
+    pixelSize: 5,
+    alienContent: [],
+    generateAlien: function () {
+        for (var y = 0; y < this.numPixels; y++) {
+            for (var x = 0; x < this.numPixels; x++) {
+                if (x < Math.round(this.numPixels / 2)) {
+                    this.alienContent[x + y * this.numPixels] = Math.round(Math.random());
+                }
+                else {
+                    this.alienContent[x + y * this.numPixels] =
+                        this.alienContent[(this.numPixels - x - 1) + y * this.numPixels];
+                }
+            }
+        }
+    },
+    renderAlien: function () {
+        for (var i = 0; i < this.numPixels * this.numPixels; i++) {
+            if (this.alienContent[i] != 0) {
+                var x = i % this.numPixels;
+                var y = Math.floor(i / this.numPixels);
+                ctx.fillRect(x * this.pixelSize, y * this.pixelSize, this.pixelSize, this.pixelSize);
+            }
+        }
+    }
+};
+
+generator.generateAlien();
+generator.renderAlien();
